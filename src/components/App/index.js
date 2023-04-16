faad158e126500d7e2cbfd1ebc80dcb2ee0c5204
Sarah Salmean <sarahsalmean@hotmail.com>
2023-04-16 10:06:11 +0200

    Fully functioning site

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import {useState} from "react"
 import BlogPost from "../BlogPost";
-import Comment from "../Comment";
 import CommentForm from "../CommentForm";
 import CommentList from "../CommentList";
 
@@ -18,34 +17,39 @@ function App() {
     imageAlt: "",
   }
 
-  const comments = [
+  const [comments, setComments] = useState([
     {author: "Harry Smith",
 comment: "Great blog, slut!",
 initials: "HS"},
 {author: "Hope Sutherland", comment:"Hello!", initials: "HS"}
-]
+])
 
-/* Todo 
-- set comment state to be array of comments 
-- have input fields recognise values
-- hook up button
-- upon click, add inputs as object to state array
-- also run initials function and add those as initials 
-- also clear input 
-*/
+const [person, setPerson] = useState("")
+const [comment, setComment] = useState("")
 
-/* var getInitials = function (comments) {
-  const names = comments[index].author.split(' ')
-     const initials = names[index].substring(0, 1).toUpperCase();
+function handlePersonChange(event) {
+  setPerson(event.target.value);
+}
+
+function handleCommentChange(event) {
+  setComment(event.target.value);
+}
+
+let initials = ""
+
+function getInitials (person) {
+  const names = person.split(' ')
+     initials = names[0].substring(0, 1).toUpperCase();
       initials = initials + names[names.length -1].substring(0, 1).toUpperCase();
-      return <h2>{initials}</h2>;
-    } */ 
+      return initials}
 
-    /*function getInitials(comments)  
-    {comments.map(x, index => {const names = comment.author.split(' ')
-     let initials = names[index].substring(0, 1).toUpperCase();
-      initials = initials + names[names.length -1].substring(0, 1).toUpperCase();
-      return <h2>{initials}</h2>;})} */
+
+function addItem(person, comment) {
+  getInitials(person)
+  setComments((comments) => [...comments, {"author": person, "comment": comment, "initials": initials,}])
+  setPerson("");
+  setComment("");
+}
 
   return <> 
   <BlogPost
@@ -59,7 +63,7 @@ initials: "HS"},
   <h3>Comments:</h3>
   <CommentList text={comments}>
   </CommentList>
-  <CommentForm></CommentForm>
+  <CommentForm personChange={handlePersonChange} commentChange={handleCommentChange} personValue={person} commentValue= {comment} onClick={() => addItem(person, comment)}> </CommentForm>
 </>
   }
 
--- src/components/CommentForm/index.js
+++ src/components/CommentForm/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import "./form.css"
 
-export default function CommentForm() {
+export default function CommentForm(props) {
   return <div class="contain"><h3>Leave a Comment:</h3>
   <h3>Name:</h3>
-  <input type="text" id="name" class="input"></input>
+  <input type="text" id="name" class="input" onChange={props.personChange} value={props.personValue}></input>
   <h3>Comment</h3>
-  <input type="text" id="comment" class="input"></input>
-  <button class="button">Submit Comment</button></div>;
+  <input type="text" id="comment" class="input" onChange={props.commentChange} value={props.commentValue}></input>
+  <button class="button" onClick={props.onClick}>Submit Comment</button></div>;
 }
 
